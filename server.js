5f42284f1e4d9a46a572af13e071627b11d2e671
Bryce Sampson <bnicerrr@gmail.com>
2022-01-30 10:12:04 -0500

    add route for gretzky career gamelog

--- server.js
+++ server.js
@@ -5,6 +5,34 @@ const PORT = 6969;
 const API_URL = 'https://statsapi.web.nhl.com/api/v1'
 const CURRENT_SEASON = '20212022'
 
+const seasons = [
+    "19791980",
+    "19801981",
+    "19811982",
+    "19821983",
+    "19831984",
+    "19841985",
+    "19851986",
+    "19861987",
+    "19871988",
+    "19881989",
+    "19891990",
+    "19901991",
+    "19911992",
+    "19921993",
+    "19931994",
+    "19941995",
+    "19951996",
+    "19961997",
+    "19961997",
+    "19971998",
+    "19981999",
+  ]
+
+const gretzkyId = '8447400'
+
+const gretzkyStats = []
+
 // get all teams
 app.get('/teams', async (req, res) => {
     try {
@@ -92,6 +120,40 @@ app.get('/player/:id/stats/game-log', async (req, res) => {
         res.send(data.stats[0].splits)
     } catch (error) {
         console.log(error.message)
+    }
+})
+
+app.get('/gretzky-gamelogs', async (req, res) => {
+    let carreer_goals = 0
+    let carreer_assists = 0
+    let carreer_points = 0
+    try {
+        // for each season return the game log and save it to gretzkyStats
+        seasons.forEach(async season => {
+            let { data } = await axios.get(`https://statsapi.web.nhl.com/api/v1/people/8447400/stats?stats=gameLog&season=${season}`)
+            gretzkyStats.push(...data.stats[0].splits)
+        })
+        
+        let sorted = gretzkyStats.sort((a, b) => b.season - a.season)
+        let gameStats = sorted.reverse().map((game, i) => {
+            carreer_goals = carreer_goals + game.stat.goals
+            carreer_assists = carreer_assists + game.stat.assists
+            carreer_points = carreer_points + game.stat.goals + game.stat.assists
+            let stats = {
+                date: game.date,
+                goals: game.stat.goals,
+                assists: game.stat.assists,
+                opponent: game.opponent.name,
+                carreer_goals: carreer_goals,
+                carreer_assists: carreer_assists,
+                carreer_point: carreer_points,
+                games_played: i
+            }
+            return stats
+            })
+        res.send(gameStats)
+    } catch (error) {
+        console.error(error.message)
     }
 })
 
